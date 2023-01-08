5de31795e4f1ef13c25b46a99bf31b8a920207d1
Mariano <mariano_farace@hotmail.com>
2023-01-08 15:56:50 +0100

    database and models setup

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        dump($request);
+        $banana = 13;
+        dump($banana);
+
         return new Response(
             <<<EOF
             <html>
